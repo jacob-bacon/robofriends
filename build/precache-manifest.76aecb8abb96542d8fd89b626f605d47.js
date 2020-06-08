0907fcfc7a2a4cbc030d55bbb79ec0437dbce416
Jacob Bacon <jake.bacn@gmail.com>
2020-06-08 04:38:30 -0500

    build files to master

---
+++ build/precache-manifest.76aecb8abb96542d8fd89b626f605d47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edfa18048b1bf09b9df3af9c4bf0d45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bb08595f74ad68506f02",
+    "url": "/static/css/main.4b2dd820.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb08595f74ad68506f02",
+    "url": "/static/js/main.0265ae82.chunk.js"
+  },
+  {
+    "revision": "fdb66bf0534857f19626",
+    "url": "/static/js/runtime-main.8f3ae637.js"
+  }
+]);
