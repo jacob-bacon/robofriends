81701b0f3120dd73957992ff2ef0f7a5ee3e7dde
Jacob Bacon <jake.bacn@gmail.com>
2020-06-08 06:04:41 -0500

    edit fetch cors setting

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,9 +13,10 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
+        fetch('https://jsonplaceholder.typicode.com/users', {mode: 'no-cors'})
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
+            .then(users => this.setState({ robots: users })
+        );
     }
 
     onSearchChange = (event) => {
