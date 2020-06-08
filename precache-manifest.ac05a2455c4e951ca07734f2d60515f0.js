bdf8ad312b17805461c94d6f3456e1326bae9350
Jacob Bacon <jake.bacn@gmail.com>
2020-06-08 05:37:12 -0500

    Updates

---
+++ precache-manifest.ac05a2455c4e951ca07734f2d60515f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10dfbd3326a8835a08f40359a3ad5b94",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd4a23efbbb1fa9776a7",
+    "url": "/robofriends/static/css/main.4b2dd820.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4a23efbbb1fa9776a7",
+    "url": "/robofriends/static/js/main.58daba75.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
