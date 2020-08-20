e63ce769b91031a71fedc9cf6d72320d16d1d77b
Jacob Bacon <jake.bacn@gmail.com>
2020-08-20 05:12:50 -0500

    Implement redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 import Scroll from '../components/Scroll';
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchField: ""
-        }
-    }
-
+    
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users })
-        );
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
+        this.props.onRequestRobots();
     }
 
     render() {
-        const {robots, searchField} = this.state;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
         //ternary
-        return !robots.length ?
+        return isPending ?
             <div className="tc grid sticky">
                 <h1>Loading...</h1>
             </div> :
@@ -37,7 +41,7 @@ class App extends Component {
             <div className="tc">
                 <div className="sticky bg-blue-gray-dark">
                     <h1 className="f3">ROBOFRIENDS</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
+                    <SearchBox searchChange={onSearchChange}/>
                 </div>
                 <Scroll>
                     <CardList robots={filteredRobots}/>
@@ -47,4 +51,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
